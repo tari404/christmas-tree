f75c51bdb81368cdef1cbb071754b6ed36ca2bf0
zihuatanejp <aimilixi@gmail.com>
2018-12-21 17:23:51 +0800

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
 import Lamp from '@/components/Lamp'
 import DrawLamp from '@/components/DrawLamp'
 import axios from 'axios'
-
+import wx from "weixin-js-sdk"
 import config from '../config.json'
 
 let web3t, contract
@@ -217,6 +217,61 @@ export default {
           setTimeout(() => {
             this.status = ''
           }, 2000)
+        })
+      })
+    },
+    // 向微信分享菜单注入分享内容
+    injectWxShareMenu({shareUrl,shareTitle,shareDescr,shareIcon}) {
+      let signUrl = encodeURIComponent(location.href.split("#")[0])
+      wx.config({
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: '', // 必填，公众号的唯一标识
+        timestamp: '', // 必填，生成签名的时间戳
+        nonceStr: '', // 必填，生成签名的随机串
+        signature: '', // 必填，签名
+        jsApiList: ["onMenuShareAppMessage", "onMenuShareTimeline", "onMenuShareQQ"] 
+      })
+      wx.ready(() => {
+        wx.onMenuShareAppMessage({
+          title: shareTitle,
+          desc: decodeURIComponent(shareDescr),
+          link: shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: shareIcon, // 分享图标
+          success: ()=> {
+            // 用户点击了分享后执行的回调函数
+            alert("分享微信好友成功")
+          },
+          cancel: ()=> {
+            // 用户取消分享后执行的回调函数
+            alert("取消分享")
+          }
+        })
+        wx.onMenuShareTimeline({
+          title: shareTitle,
+          link: shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: shareIcon, // 分享图标
+          success: ()=> {
+            // 用户点击了分享后执行的回调函数
+            alert("分享微信好友成功")
+          },
+          cancel: ()=> {
+            // 用户取消分享后执行的回调函数
+            alert("取消分享")
+          }
+        })
+        wx.onMenuShareQQ({
+          title: shareTitle, 
+          desc: decodeURIComponent(shareDescr),
+          link: shareUrl,
+          imgUrl: shareIcon, 
+          success: ()=> {
+            // 用户确认分享后执行的回调函数
+            alert("分享QQ好友成功")
+          },
+          cancel: ()=> {
+            // 用户取消分享后执行的回调函数
+            alert("取消分享")
+          }
         })
       })
     }
